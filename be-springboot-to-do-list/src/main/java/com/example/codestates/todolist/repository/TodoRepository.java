fd148680ae5215e455622fb5df2776f677fcb813
sooani <118452650+sooani@users.noreply.github.com>
2023-12-21 18:09:23 +0900

    Delete be-springboot-to-do-list directory

--- be-springboot-to-do-list/src/main/java/com/example/codestates/todolist/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codestates.todolist.repository;
-
-import com.example.codestates.todolist.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
--- be-springboot-to-do-list/src/test/java/com/example/demo/todolisttest/ControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.todolisttest;
-
-public class ControllerTest {
-}
